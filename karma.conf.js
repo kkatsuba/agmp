f6794622d73bcc92aa8e52a232878af641b55651
Kiryl Katsuba <katsuba1995@gmail.com>
2018-06-28 13:08:29 +0300

    add travic-ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,26 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const chromeHeadless = {
+  autoWatch: false,
+  browsers: ['ChromeHeadless'],
+  customLaunchers: {
+    ChromeHeadlessNoSandbox: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox']
+    }
+  },
+  singleRun: true
+};
+
+const chrome = {
+  autoWatch: true,
+  browsers: ['Chrome'],
+  singleRun: false
+};
+
+const browsers = process.env.NODE_ENV === 'local' ? chrome : chromeHeadless;
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -24,8 +44,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    ...browsers
   });
 };
