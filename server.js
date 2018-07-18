92e4478069a0f7716394110c2b6af31987fd3c5e
Kiryl Katsuba <katsuba1995@gmail.com>
2018-07-18 13:29:17 +0300

    asdas

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,10 +9,20 @@ app.use(express.static(path.join(__dirname, '/dist/agmp')));
 
 app.get('*', function(request, response) {
   console.log(path.join(__dirname, '/dist/agmp/index.html'));
-    response.sendFile(path.join(__dirname, '/dist/agmp/index.html'));
+
+  fs.readFile(path.join(__dirname, '/dist/agmp/index.html'), (err, data) => {
+    console.log(err)
+    console.log(data.toString());
+  });
+
+  fs.readdir(path.join(__dirname, '/dist/agmp'), (err, files) => {
+    console.log(files)
+  })
+
+  response.sendFile(path.join(__dirname, '/dist/agmp/index.html'));
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log(`Node app is running on http://localhost:${app.get('port')}`);
 });
 
